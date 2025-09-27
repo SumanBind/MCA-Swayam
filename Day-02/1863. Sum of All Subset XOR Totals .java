2f67355b7e5e3e764f3a28b3924354d309ff3ff2
Suman Bind <Sumangic0001@gmail.com>
2025-09-27 12:09:42 +0530

    Create 1863. Sum of All Subset XOR Totals .java

---
+++ Day-02/1863. Sum of All Subset XOR Totals .java
@@ -0,0 +1,12 @@
+class Solution 
+{
+    public int subsetXORSum(int[] nums) 
+    {
+        int sum=0;
+        for(int num:nums)
+        {
+            sum|=num;
+        }
+        return sum<<(nums.length-1);
+    }
+}

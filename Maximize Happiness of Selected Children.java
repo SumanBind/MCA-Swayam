af938af603479000877abc1d83873a3ffd71ff58
Suman Bind <Sumangic0001@gmail.com>
2025-12-25 20:42:58 +0530

    Implement maximum happiness sum calculation

---
+++ Maximize Happiness of Selected Children.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public long maximumHappinessSum(int[] happiness, int k) 
+    {
+        Arrays.sort(happiness);
+        long sum=0;
+        int dec=0;
+        for(int i=happiness.length-1;i>=0&&k>0;i--,k--)
+        {
+            int curr=happiness[i]-dec;
+            if(curr>0)
+            {
+                sum+=curr;
+            }
+            dec++;
+        }
+        return sum;
+    }
+}

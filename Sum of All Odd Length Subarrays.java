7adc7d3b394c76c9dbc49dc190c16478e23f9b45
Suman Bind <Sumangic0001@gmail.com>
2026-01-18 18:52:15 +0530

    Implement sum of all odd length subarrays

---
+++ Sum of All Odd Length Subarrays.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int sumOddLengthSubarrays(int[] arr) 
+    {
+    int n = arr.length;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+     {
+        int left = i + 1;
+        int right = n - i;
+
+        int oddCount = (left * right + 1) / 2;
+        sum += arr[i] * oddCount;
+      }
+    return sum;
+    }
+}

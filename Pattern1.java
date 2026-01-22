c9b9aa8f76c5db3f9c58c829342367c8a9041b18
Suman Bind <Sumangic0001@gmail.com>
2026-01-22 15:00:48 +0530

    Add pattern printing program in Pattern1.java
    This program takes an integer input and prints a specific pattern using '@' and '*' characters.

---
+++ Pattern1.java
@@ -0,0 +1,61 @@
+/*Enter a Number :
+9
+    @@@@@@@@@
+     @@@@@@@
+      @@@@@
+       @@@
+        @
+*********
+*       *
+*       *
+*       *
+*       *
+*       *
+*       *
+*       *
+*       *
+*/
+
+
+
+
+import java.util.*;
+import java.util.Scanner;
+public class Pattern1 
+{
+	public static void main(String[] args) 
+			{
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter a Number :");
+				int n=sc.nextInt();
+				for(int i=0;i<=n/2;i++)
+				{
+					for(int j=0;j<n/2+i;j++)
+					{
+						System.out.print(" ");	
+					}
+					for(int k=0;k<n-i*2;k++)
+					{
+						System.out.print("@");
+					}
+					System.out.println();
+				}
+				
+				for(int i=0;i<n;i++)
+				{
+					for(int j=0;j<n;j++)
+					{
+						if(i==0||j==0||j==n-1)
+						{
+							System.out.print("*");
+						}
+						else
+						{
+							System.out.print(" ");
+						}	
+					}
+					
+					System.out.println();
+				}
+			}
+}

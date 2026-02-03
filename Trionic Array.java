15a4b79189c4e8530dea1dc40e0765d77e87cbf9
Suman Bind <Sumangic0001@gmail.com>
2026-02-03 18:02:32 +0530

    Add trionic array check implementation
    Implement a method to check if an array is trionic based on specified conditions.

---
+++ Trionic Array.java
@@ -0,0 +1,87 @@
+/*You are given an integer array nums of length n.
+
+An array is trionic if there exist indices 0 < p < q < n − 1 such that:
+
+nums[0...p] is strictly increasing,
+nums[p...q] is strictly decreasing,
+nums[q...n − 1] is strictly increasing.
+Return true if nums is trionic, otherwise return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,2,6]
+
+Output: true
+
+Explanation:
+
+Pick p = 2, q = 4:
+
+nums[0...2] = [1, 3, 5] is strictly increasing (1 < 3 < 5).
+nums[2...4] = [5, 4, 2] is strictly decreasing (5 > 4 > 2).
+nums[4...5] = [2, 6] is strictly increasing (2 < 6).
+Example 2:
+
+Input: nums = [2,1,3]
+
+Output: false
+
+Explanation:
+
+There is no way to pick p and q to form the required three segments.
+
+ */
+
+
+
+
+
+import java.util.*;
+
+class Main 
+{
+    public boolean isTrionic(int[] nums,int n) 
+    {
+        int i=0;
+        while(i+1<n&&nums[i]<nums[i+1])
+        {
+            i++;
+        }
+        if(i==0)
+        {
+           return false;
+        }
+       while(i+1<n&&nums[i]>nums[i+1])
+        {
+            i++;
+        }
+        if(i==0||i==n-1)
+        {
+           return  false;
+        } 
+        while(i+1<n&&nums[i]<nums[i+1])
+        {
+            i++;
+        }
+        return i==n-1;
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array length");
+        int n=sc.nextInt();
+        int a[] = new int[n];
+
+        System.out.println("Array elements:");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Main ob=new Main();
+        
+        System.out.println(ob.isTrionic(a,n));
+    }
+}

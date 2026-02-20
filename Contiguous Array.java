71995b36a63fec14fc78967cc8d726542ab2b7fb
Suman Bind <Sumangic0001@gmail.com>
2026-02-20 22:30:34 +0530

    Implement solution to find maximum length of contiguous array

---
+++ Contiguous Array.java
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public int findMaxLength(int[] nums) 
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+    
+        map.put(0, -1);
+        
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (nums[i] == 0) 
+            {
+                sum += -1;
+            }
+             else 
+            {
+                sum += 1;
+            }
+            
+            if (map.containsKey(sum)) 
+            {
+                int length = i - map.get(sum);
+                maxLength = Math.max(maxLength, length);
+            } 
+            else 
+    {
+                map.put(sum, i);
+            }
+        }
+        
+        return maxLength;
+    }
+}

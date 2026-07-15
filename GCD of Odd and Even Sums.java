acc06175a79e473816d46b6d73ed77e8901e62a1
Suman Bind <Sumangic0001@gmail.com>
2026-07-15 16:04:02 +0530

    Implement GCD of Odd and Even Sums calculation
    Calculates the GCD of the sums of the smallest n positive odd and even numbers.

---
+++ GCD of Odd and Even Sums.java
@@ -0,0 +1,62 @@
+/*
+
+You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+
+sumOdd: the sum of the smallest n positive odd numbers.
+
+sumEven: the sum of the smallest n positive even numbers.
+
+Return the GCD of sumOdd and sumEven.
+
+ 
+
+Example 1:
+
+Input: n = 4
+
+Output: 4
+
+Explanation:
+
+Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4.
+
+Example 2:
+
+Input: n = 5
+
+Output: 5
+
+Explanation:
+
+Sum of the first 5 odd numbers sumOdd = 1 + 3 + 5 + 7 + 9 = 25
+Sum of the first 5 even numbers sumEven = 2 + 4 + 6 + 8 + 10 = 30
+Hence, GCD(sumOdd, sumEven) = GCD(25, 30) = 5.*/
+
+class Solution 
+{
+    public int gcdOfOddEvenSums(int n) 
+    {
+        int a=0;
+        int b=0;
+        for(int i=1;i<2*n+1;i++)
+        {
+            if(i%2==0)
+            {
+                a=a+i;
+            }
+            else
+            {
+                b=b+i;
+            }
+        }
+        while (b != 0) 
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
